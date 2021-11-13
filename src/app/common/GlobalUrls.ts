6f7404f8e8f317f6b2d0bb30fad667c998143478
Alvaro Atias <aloatias@hotmail.com>
2021-11-13 19:41:09 +0100

    wip

--- src/app/common/GlobalUrls.ts
+++ src/app/common/GlobalUrls.ts
@@ -1,6 +1,6 @@
 export class GLobalUrls {
-    public static getContenders= "/Cat/Contenders";
-    public static getVotes = "/Cat/Winners";
-    public static getVotesCount = "/Challenge/TotalVotes";
-    public static setChallengeResult = "/Challenge";
+    public static getContenders= "/cat/Contenders";
+    public static getVotes = "/cat/Winners";
+    public static getVotesCount = "/challenge/TotalVotes";
+    public static setChallengeResult = "/challenge";
 }
