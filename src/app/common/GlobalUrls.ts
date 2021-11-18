32d5173c4397da4d43464660d9312b9bd3d27167
aloatias <aloatias@hotmail.com>
2021-11-18 11:44:10 +0100

    GlobalUrls modifications

--- src/app/common/GlobalUrls.ts
+++ src/app/common/GlobalUrls.ts
@@ -1,6 +1,6 @@
 export class GLobalUrls {
-    public static getContenders= "/cat/contenders";
-    public static getVotes = "/cat/winners";
-    public static getVotesCount = "/challenge/total-votes";
-    public static setChallengeResult = "/challenge";
+    public static getContenders = "/api/cat/contenders";
+    public static getVotes = "/api/cat/winners";
+    public static getVotesCount = "/api/challenge/total-votes";
+    public static setChallengeResult = "/api/challenge";
 }
